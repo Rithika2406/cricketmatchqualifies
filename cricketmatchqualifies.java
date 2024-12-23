d04c3e31545111d3008f8d6e85417e757a9ef291
Rithika P <166213945+Rithika2406@users.noreply.github.com>
2024-12-23 21:34:51 +0530

    Add files via upload

---
+++ cricketmatchqualifies.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	   Scanner S=new Scanner(System.in);
+	   int x=S.nextInt();
+	   if(x>=12)
+	   {
+	       System.out.println("Yes");
+	   }
+	   else{
+	       System.out.println("No");
+	   }
+
+	}
+}
